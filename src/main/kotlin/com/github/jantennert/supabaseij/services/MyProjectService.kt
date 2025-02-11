2ccaa9744333d32755f64fe5eed1f1bfe5d978a2
GitHub Action <action@github.com>
2025-02-11 15:42:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/jantennert/supabaseij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jantennert/supabaseij/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jantennert.supabaseij.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jantennert.supabaseij.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
